01e2ceca413b4937d37cbdcd54f806e4730fdebf
chenyang <644093581@qq.com>
2023-01-30 17:27:31 +0800

    refactor: supporting real hardware 3A5000

--- elfloader-tool/src/drivers/uart/loongson3A5000-uart.c
+++ elfloader-tool/src/drivers/uart/loongson3A5000-uart.c
@@ -19,14 +19,22 @@
 #define UART_REG_LSR 0x05
 #define UART_REG_MSR 0x06
 
+#define UART_REG_LSR_TE BIT(6)
 #define UART_REG_LSR_TFE BIT(5)
 
-#define UART_REG(mmio, x) ((volatile uint8_t *)(((unsigned long)(mmio) + (x))|0x6000000000000000))
+#define UART_REG(mmio, x) ((volatile uint8_t *)(((unsigned long)(mmio) + (x)) | 0x6000000000000000))
 
 static int loongson3A5000_uart_putchar(struct elfloader_device *dev, unsigned int c)
 {
     volatile void *mmio = dev->region_bases[0];
-    while (!(*UART_REG(mmio, UART_REG_LSR) & UART_REG_LSR_TFE));
+    while (!(*UART_REG(mmio, UART_REG_LSR) & UART_REG_LSR_TE));
+    asm volatile(
+        "csrwr  $t0, 0x34  \n"
+        "csrwr  $t1, 0x35  \n"
+        "li.d  $t1, 0x1fe001e5  \n"
+        "iocsrrd.b $t0, $t1  \n"
+        "csrrd  $t0, 0x34  \n"
+        "csrrd  $t1, 0x35  \n");
     *UART_REG(mmio, UART_REG_DAT) = (c & 0xff);
 
     return 0;
